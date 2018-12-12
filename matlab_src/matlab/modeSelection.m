ff82312fb11110425f6b91f69d8bea4a2924eccd
dbanyay <banyaydani@gmail.com>
2018-12-12 16:36:58 +0100

    wrong rate calc

--- matlab_src/matlab/modeSelection.m
+++ matlab_src/matlab/modeSelection.m
@@ -1,4 +1,4 @@
-function decisionMatrix = modeSelection(qDCT16, rate16)
+function decisionMatrix = modeSelection(qDCT16, bRate, FPS)
 % Select encoding mode for each 16*16 block
 
 numOfBlocks = size(qDCT16,3);
@@ -8,18 +8,20 @@ numOfQuantLevels = size(qDCT16,5);
 decisionMatrix = zeros(numOfBlocks, numOfFrames, numOfQuantLevels);
 
 for block = 1 : numOfBlocks
-    for frame = 1:numOfFrames
-        for quant = 1:numOfQuantLevels            
-            
+    for frame = 2:numOfFrames
+        for quant = 1:numOfQuantLevels
+                                   
             stepSize = (2^quant)^2;
             lambda = 0.2*stepSize^2; % Lagrange multiplier
-
-            D0 = immse(qDCT(:,:,block,frame,1),qDCT(:,:,block,frame,quant));
-            R0 = rate16(block,frame,quant);
+            
+            % intra mode
+            D0 = immse(qDCT16(:,:,block,frame,1),qDCT16(:,:,block,frame,quant));
+            R0 = bRate(quant)/numOfBlocks/FPS;
             R0 = R0 +1; % adding 1 bit for copy flag
             J0 = D0 + lambda*R0;
-
-            D1 = immse(qDCT(:,:,block,frame,1),qDCT(:,:,block,frame,quant));
+            
+            % copy mode
+            D1 = immse(qDCT16(:,:,block,frame-1,quant),qDCT16(:,:,block,frame,1));
             R1 = 1; % copy flag
             J1 = D1 + lambda*R1;
 
