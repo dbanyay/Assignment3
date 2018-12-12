59aa0d408944f4569d621c787b0725e990370cb2
dbanyay <banyaydani@gmail.com>
2018-12-12 15:10:03 +0100

    merge2

--- matlab_src/matlab/modeSelection.m
+++ matlab_src/matlab/modeSelection.m
@@ -1,5 +1,16 @@
-function decisions = modeSelection(blocks16)
+function decisions = modeSelection(blocks)
 % Select encoding mode for each 16*16 block
+
+numframes = size(blocks,4);
+numblocks = size(blocks,3);
+
+
+curblock = blocks(:,:,1,1);
+prevblock = blocks(:,:,1,2);
+
+
+
+
 
 
 end
